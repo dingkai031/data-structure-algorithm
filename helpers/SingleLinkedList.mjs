621edb8c7ddb23d1a4fe3ee308ebb852c36e0ce3
Yovan Julio <yovanjulioadam@gmail.com>
2024-01-02 02:05:11 +0700

    added remove node by data name method for single linked list

--- helpers/SingleLinkedList.mjs
+++ helpers/SingleLinkedList.mjs
@@ -40,12 +40,53 @@ export default class SingleLinkedList {
     }
   }
 
+  removeNode(data) {
+    // initiate 2 pointer
+    let currentNode = this.head;
+    let previousNode = currentNode;
+
+    // check if the list is empty
+    if (!currentNode) return;
+    // if the list only contains 1 node, call removeHead()
+    if (currentNode.data === data) return this.removeHead();
+
+    // travel both of the pointer until the current node is null
+    // and node is found
+    while (currentNode && currentNode.data !== data) {
+      previousNode = currentNode;
+      currentNode = currentNode.getNextNode();
+    }
+
+    // if data was not found return an error
+    if (!currentNode || currentNode.data !== data)
+      throw new Error("node not found");
+    // if the node that contains data that we looking for,
+    // call removeTail()
+    if (!currentNode.getNextNode()) return this.removeTail();
+
+    // if the data is found, and the data is not inside the first and the last
+    // node. Set the previous node next property to current node next property
+    previousNode.setNextNode(currentNode.getNextNode());
+  }
+
   removeHead() {
     const removedHead = this.head;
 
     if (!removedHead) return;
     this.head = this.head.getNextNode();
     return `removed node ${removedHead.data}`;
+  }
+
+  removeTail() {
+    let currentNode = this.head;
+    if (!currentNode) return;
+    if (!currentNode.getNextNode()) {
+      return (this.head = currentNode.getNextNode());
+    }
+    while (currentNode.getNextNode().getNextNode()) {
+      currentNode = currentNode.getNextNode();
+    }
+    currentNode.next = null;
   }
 
   printList() {
--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import SingleLinkedList from "./helpers/SingleLinkedList.mjs";
 const food = new SingleLinkedList();
 food.addToHead("mi goreng");
 food.addToHead("mi kuah");
+
+// food.removeNode("mi bihun"); //will throw an error
 food.printList();
